47af8a600dcd2214a7e3195ae919ed343f9a0e45
宿梓航 <Ferdinandsukhoi@outlook.com>
2020-10-27 19:41:28 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Coalesce-My-Source-Code
+
+To coalesce all source files in a directory into a single file, to paste in your report.
+
+Based On .NET Core 3.1 & MobileSuit 2.2
+
+## Install
+
+Run `dotnet tool install iFerS.Coalesce-My-Source-Code --global`
+
+If you have old version installed, run `dotnet tool uninstall iFerS.Coalesce-My-Source-Code --global` first.
+
+## Usage
+
+Run `cmsc -h` to see usage.
---
+++ src/CoalesceMySourceCode.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using PlasticMetal.MobileSuit;
+using PlasticMetal.MobileSuit.Core;
+using PlasticMetal.MobileSuit.ObjectModel;
+using PlasticMetal.MobileSuit.ObjectModel.Future;
+using PlasticMetal.MobileSuit.Parsing;
+
+namespace CoalesceMySourceCode
+{
+    [SuitInfo("CoalesceMySourceCode")]
+    class CoalesceMySourceCode 
+        : CommandLineApplication<CoalesceMySourceCode.CoalesceParameter>,
+        ICommandLineApplication
+            , IStartingInteractive
+    {
+        public class CoalesceParameter : AutoDynamicParameter
+        {
+            [Option("o")] public string OutputFileName { get; set; } = "";
+            [Option("i")] public string InputDirectory { get; set; } = "";
+            [Option("f")]
+            [WithDefault]
+            [AsCollection]
+            public List<string> Format { get; set; } = new List<string>();
+            [Option("ex")]
+            [AsCollection]
+            [WithDefault]
+            public List<string> Excludes { get; set; } = new List<string>();
+        }
+        [SuitAlias("cc")]
+        public int Coalesce(CoalesceParameter p)
+        {
+            var sb = new StringBuilder();
+            CoalesceDir("", new DirectoryInfo(p.InputDirectory), sb, p);
+            File.WriteAllText(p.OutputFileName, sb.ToString());
+            return 0;
+        }
+        
+        private void CoalesceDir(string prefix, DirectoryInfo dir, StringBuilder sb,  CoalesceParameter p)
+        {
+            foreach (var f in dir.GetFiles())
+            {
+                if (p.Format.Contains(f.Extension))
+                {
+                    var fn = $"{prefix}{dir.Name}\\{f.Name}";
+                    if (p.Excludes.Any(ex=> Regex.IsMatch(fn,ex)))
+                    {
+                        IO.WriteLine(
+                            Suit.CreateContentArray(
+                                ("Skipped:",IO.ColorSetting.ErrorColor),
+                                (fn,null)
+                            ));
+                        continue;
+                    }
+                    IO.WriteLine(
+                        Suit.CreateContentArray(
+                            ("Added:", IO.ColorSetting.AllOkColor),
+                            (fn, null)
+                        ));
+                    sb.Append($"\n\n// {fn}\n\n");
+                    sb.Append(File.ReadAllText(f.FullName));
+                }
+            }
+
+            foreach (var nextDir in dir.GetDirectories())
+            {
+                CoalesceDir(prefix + $"{dir.Name}\\", nextDir, sb, p);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Suit.GetBuilder()
+                .UsePrompt<PowerLineThemedPromptServer>()
+                .UseLog(PlasticMetal.MobileSuit.Core.ILogger.OfFile("D:\\clc.log"))
+                .Build<CoalesceMySourceCode>()
+                .Run(args);
+        }
+
+        public override void SuitShowUsage()
+        {
+            IO.WriteLine(Suit.CreateContentArray(
+                ("Usage:",IO.ColorSetting.ListTitleColor)
+                
+            ));
+            IO.WriteLine("\tcmsc -i <InputDir> -o <OutputFile> -f <extension>", IO.ColorSetting.CustomInformationColor);
+            IO.WriteLine(Suit.CreateContentArray(
+                ("Options:", IO.ColorSetting.ListTitleColor)
+            ));
+            IO.AppendWriteLinePrefix();
+            IO.WriteLine(Suit.CreateContentArray(
+                
+                ("-f <extension>", IO.ColorSetting.CustomInformationColor),
+                ("\tAllow Multiple, At least One; The extension of files' name that you want to coalesce.", null)
+            ));
+            IO.WriteLine(Suit.CreateContentArray(
+
+                ("-ex <RegEx>", IO.ColorSetting.CustomInformationColor),
+                ("\tAllow Multiple; The file/dir names' RegEx which you want to exclude.", null)
+            ));
+            IO.WriteLine();
+
+
+            IO.SubtractWriteLinePrefix();
+        }
+
+        public override int SuitStartUp(CoalesceParameter arg)
+        {
+            IO.WriteLine("Called.");
+            return 0;
+        }
+        [SuitIgnore]
+        public void OnInitialized()
+        {
+            IO.PrintAssemblyInformation(
+                "Coalesce-My-Source-Code",
+                new Version("1.0.0"),
+                true,
+                "Ferdinand Sukhoi",
+                "https://github.com/FerdinandSukhoi/Coalesce-My-Source-Code",
+                false
+            );
+        }
+    }
+}
